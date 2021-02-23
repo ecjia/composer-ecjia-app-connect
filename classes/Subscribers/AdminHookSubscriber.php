101082d7f5aeeb8fb639329dddd97bf65fa37ec0
王正东 <wangzhengdong@ecmoban.com>
2021-02-23 14:21:43 +0800

    移除帐号连接菜单

--- classes/Subscribers/AdminHookSubscriber.php
+++ classes/Subscribers/AdminHookSubscriber.php
@@ -13,12 +13,12 @@ use Ecjia\App\Client\ApplicationConfigFactory\ApplicationConfigOptions;
 
 class AdminHookSubscriber
 {
-    public static function connect_admin_menu_api($menus)
-    {
-        $menu = ecjia_admin::make_admin_menu('menu_user_connect', __('账号连接', 'connect'), RC_Uri::url('connect/admin_plugin/init'), 52)->add_purview('connect_users_manage');
-        $menus->add_submenu($menu);
-        return $menus;
-    }
+//    public static function connect_admin_menu_api($menus)
+//    {
+//        $menu = ecjia_admin::make_admin_menu('menu_user_connect', __('账号连接', 'connect'), RC_Uri::url('connect/admin_plugin/init'), 52)->add_purview('connect_users_manage');
+//        $menus->add_submenu($menu);
+//        return $menus;
+//    }
 
 //    public static function add_maintain_command($factories)
 //    {
@@ -36,7 +36,7 @@ class AdminHookSubscriber
     public function subscribe(Dispatcher $events)
     {
 
-        RC_Hook::add_filter('user_admin_menu_api', array(__CLASS__, 'connect_admin_menu_api') );
+//        RC_Hook::add_filter('user_admin_menu_api', array(__CLASS__, 'connect_admin_menu_api') );
 //        RC_Hook::add_action('ecjia_maintain_command_filter', array(__CLASS__, 'add_maintain_command'));
 
     }
